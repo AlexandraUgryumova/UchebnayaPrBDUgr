5a5326ccd4bcddcedbdf7f2d9d1a1d8750224713
AlexandraUgryumova <124123433+AlexandraUgryumova@users.noreply.github.com>
2023-05-25 11:52:39 +0500

    initial commit

---
+++ WorkDB_Ugryumova/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WorkDB_Ugryumova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Logintxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft YaHei Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControl1.Location = new System.Drawing.Point(123, 51);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(228, 300);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.GhostWhite;
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 30);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(220, 266);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "sing in";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.HotPink;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(6, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Sing in";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(202, 29);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 21);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Password";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Login";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.GhostWhite;
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.Logintxt);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 30);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(220, 266);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "sing up";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.HotPink;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(6, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Sing up";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(2, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(201, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Logintxt
+            // 
+            this.Logintxt.Location = new System.Drawing.Point(2, 27);
+            this.Logintxt.Name = "Logintxt";
+            this.Logintxt.Size = new System.Drawing.Size(201, 29);
+            this.Logintxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 384);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(761, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(751, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 21);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Confirmation";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(3, 139);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(197, 29);
+            this.textBox4.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(425, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 253);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(824, 562);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Logintxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WorkDB_Ugryumova/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WorkDB_Ugryumova
+{
+    public partial class Form1 :Form
+    {
+        DateTime dt = new DateTime( );
+
+        SqlDataAdapter adptr;
+        DataTable table;
+        SqlConnection connect = new SqlConnection(@"Data Source=PC325L12\SQLEXPRESS;Initial Catalog=SecurityDB_Ugryumova;Integrated Security=True");
+        public Form1 ()
+        {
+            InitializeComponent( );
+            TableGridView( );
+            pictureBox1.Image = Image.FromFile("mouse.png");
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            Application.Exit( );
+        }
+
+        private void dataGridView1_CellContentClick (object sender, DataGridViewCellEventArgs e)
+        {
+        }
+        private void TableGridView ()
+        {
+            connect.Open( );
+            adptr = new SqlDataAdapter("select * from User_tbl", connect);
+            table = new DataTable( );
+            adptr.Fill(table);
+            dataGridView1.DataSource = table;
+            connect.Close( );
+        }
+
+        private void button2_Click (object sender, EventArgs e)
+        {
+            dt = DateTime.Today;
+            string log = Logintxt.Text;
+            string pass = textBox2.Text;
+            if ( log == "" || pass == "" )
+            {
+                MessageBox.Show("заполнены не все поля", "сообщение");
+            }
+            else
+            {
+                connect.Open( );
+                string prov = $"SELECT * FROM dbo.User_tbl WHERE Login = '{log}'";
+                SqlDataAdapter adptr1 = new SqlDataAdapter(prov, connect);
+                DataTable table = new DataTable( );
+                adptr1.Fill(table);
+
+                if ( table.Rows.Count == 1 ) 
+                {
+                    MessageBox.Show("данный логин уже зарезервирован другим пользователем", "сообщение"); 
+                    connect.Close( );
+                }
+                else
+                {
+                    if ( pass == textBox4.Text )
+                    {
+                        string query = $"INSERT INTO dbo.User_tbl (login,  password,  Count,  date,  active,   role) VALUES('{log}','{pass}',0,'{dt.ToString("yyyy-MM-dd")}','True','user')";
+                        SqlDataAdapter adptr2 = new SqlDataAdapter(query, connect);
+                        DataTable table2 = new DataTable( );
+                        adptr2.Fill(table2);
+                    }
+                    else
+                    {
+                        MessageBox.Show("пароли не одинаковы", "сообщение");
+                    }
+                        connect.Close( );
+                        TableGridView( );
+                    
+                }
+            }
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+        {
+            string log = textBox1.Text;
+            string pass = textBox3.Text;
+            if ( log == "" || pass == "" )
+            {
+                MessageBox.Show("заполнены не все поля", "сообщение");
+            }
+            else
+            {
+                connect.Open( );
+                string prov = $"SELECT Role, Active,Login FROM dbo.User_tbl WHERE Login = '{log}' and Password = '{pass}'";
+                string s = $"UPDATE dbo.User_tbl set Count+=1 where Login = '{log}'";
+                string select = $"SELECT Login, Active From dbo.User_tbl WHERE Login = '{log}' and Active ='False'";
+
+                SqlDataAdapter adptr1 = new SqlDataAdapter(select, connect);
+                DataTable table = new DataTable( );
+                adptr1.Fill(table);
+                if ( table.Rows.Count == 1 )
+                {
+                    MessageBox.Show("этот пользователь заблокирован", "сообщение");
+                    connect.Close( );
+                }
+                else
+                {
+
+                    SqlDataAdapter adptr2 = new SqlDataAdapter(prov, connect);
+
+                    adptr2.Fill(table);
+
+                    if ( table.Rows.Count == 0 )
+                    {
+                        MessageBox.Show("вы ввели неверный пароль или логин, попробуйте ещё раз", "сообщение");
+                        SqlDataAdapter adptr3 = new SqlDataAdapter(s, connect);
+                        adptr3.Fill(table);
+                        string sq = $"UPDATE dbo.User_tbl set Active = 'False' where Count >=3";
+                        SqlDataAdapter adptr4 = new SqlDataAdapter(sq, connect);
+                        adptr4.Fill(table);
+                        connect.Close( );
+                    }
+                    else
+                    {
+                        if ( table.Rows [ 0 ] [ 0 ].ToString( ) == "user" )
+                        {
+                            User fr2 = new User( );
+                            this.Hide( );
+                            fr2.Show( );
+                        }
+                        if ( table.Rows [ 0 ] [ 0 ].ToString( ) == "Admin" )
+                        {
+                            Admin fr1 = new Admin( );
+                            this.Hide( );
+                            fr1.Show( );
+                        }
+                        connect.Close( );
+                    }
+                    TableGridView( );
+                }
+            }
+        }
+
+        private void label4_Click (object sender, EventArgs e)
+        {
+            MessageBox.Show("Шумкова какашка");
+        }
+    }
+}
